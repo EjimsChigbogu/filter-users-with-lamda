0f9e7d919c9b1fe146ed8ede9bee14ac9250c98c
Ejims Chigbogu <stanleychigbogu@gmail.com>
2025-05-11 10:11:12 +0100

    First time writing lambda, so i had to get creative. wrote the for loop(in the readme file) before trying to convert to the lambda syntax good experience

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # filter users with lamda
 using lambda to filter users with a balance over a certain amount
+i will be using the filter function with lamda  for this taks
+
+
+decided to writ the logic normal way before converting to lambda
+# for pin in users:
+#     if users[pin]['balance'] >= 500:
+#         print(f'{pin} - {users[pin]["balance"]}')
---
+++ filter-users.py
@@ -0,0 +1,26 @@
+"""Use lambda to filter users with a balance over a certain amount"""
+
+# Users
+users = {
+    "1234": {"name": "Alice", "balance": 500},
+    "5678": {"name": "Bob", "balance": 300},
+    "2468": {"name": "Charlie", "balance": 750},
+    "1357": {"name": "Diana", "balance": 620},
+    "1122": {"name": "Ethan", "balance": 400},
+    "3344": {"name": "Fiona", "balance": 1000},
+    "5566": {"name": "George", "balance": 150},
+    "7788": {"name": "Hannah", "balance": 920},
+    "9900": {"name": "Ivan", "balance": 0},
+    "2020": {"name": "Jade", "balance": 850},
+}
+
+rich_users = list(filter(lambda x: users[x]['balance'] >= 500, users))
+
+print('User with balance > or == to 500')
+for pin in rich_users:
+    users[pin]
+    print(f"{users[pin]['name']} - {users[pin]['balance']}")
+
+
+
+
